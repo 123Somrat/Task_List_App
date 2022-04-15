5c6ad2fe8d21f0e4d23094d04fe8b16a26a5274e
Mohammad Jafar Uddin <mdjafaruddinsomrat@gmail.com>
2022-04-16 01:54:37 +0200

    Define UI Element"

---
+++ js/index.js
@@ -0,0 +1,7 @@
+//Define UI Element
+
+let form = document.querySelector("#task_form");
+let tasklist = document.querySelector("ul");
+let clearBtn = documenty.querySelector("#clear_task_btn");
+let filter = document.querySelector("#task_filter");
+let taskInput = document.querySelector("#New-Task");
