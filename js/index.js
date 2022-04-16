849c5ade8fe5f76805264f27c1a0f0cbef0079d5
Mohammad Jafar Uddin <mdjafaruddinsomrat@gmail.com>
2022-04-16 02:04:37 +0200

    Define Event Listeners

--- js/index.js
+++ js/index.js
@@ -5,3 +5,12 @@ let tasklist = document.querySelector("ul");
 let clearBtn = documenty.querySelector("#clear_task_btn");
 let filter = document.querySelector("#task_filter");
 let taskInput = document.querySelector("#New-Task");
+
+
+//Define Event Listeners
+
+form.addEventListener("submit",addTask);
+tasklist.addEventListener("click",removeTask);
+clearBtn.addEventListener("click",clearTask);
+filter.addEventListener("keyup",filterTask);
+document.addEventListener("DomContentLoaded",getTasks);
